850579680f215f2febe958fde4ec04dc180df200
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-05 15:10:57 +0530

    Update test_capitalize.py

--- test_capitalize.py
+++ test_capitalize.py
@@ -3,5 +3,11 @@ def capitalize_string(s):
         raise TypeError('Please provide a string')
     return s.capitalize()
 
+def add(a,b):
+    return a+b;
+    
+def test_add():
+    assert add(3,7) == 10
+    
 def test_capitalize_string():
     assert capitalize_string('test') == 'Test'
